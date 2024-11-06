5756840904ce847f2d6a77bf764bedc675fb341f
Afsana Meem <afsumeem@gmail.com>
2024-11-06 21:13:11 +0600

    about content updated

--- src/components/ui/About.jsx
+++ src/components/ui/About.jsx
@@ -1,5 +1,69 @@
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          {/* tab  */}
+          <div>
+            <button>About</button>
+            <button>Experience</button>
+            <button>Contact</button>
+          </div>
+          <h3>Exceptional culinary experience and delicious food</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non
+            sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque
+            bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur
+            adipiscing elit do eiusmod tempor incididunt ut labore et dolore
+            magna minim veniam nostrud exercitation.
+          </p>
+          <div>
+            <button>About More</button>
+            <div>
+              <img src="" alt="" />
+              <p>+88 3426 739 485</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div>
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h5>Fast delivery</h5>
+            <p>Within 30 minutes</p>
+          </div>
+        </div>
+        {/*  */}
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h5>Absolute dining</h5>
+            <p>Best buffet restaurant</p>
+          </div>
+        </div>
+        {/*
+         */}
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h5>Pickup delivery</h5>
+            <p>Grab your food order</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
