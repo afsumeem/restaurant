cb918308ad3006ce72416a586a937989dd7a1226
Afsana Meem <afsumeem@gmail.com>
2024-11-09 01:31:22 +0600

    form functionality done

--- src/components/ui/BookingForm.jsx
+++ src/components/ui/BookingForm.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import styles from "../../styles/BookingForm.module.css";
+import calender from "../../assets/calender.svg";
 
 const BookingForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    date: "",
+    people: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Save form data to local storage
+    localStorage.setItem("bookingData", JSON.stringify(formData));
+    alert("Booking information saved successfully!");
+  };
+
   return (
     <div
       className={`${styles.popularFoodSection} h-[722px] lg:h-[788px] px-8 lg:px-[300px] py-[30px] lg:py-[120px]`}
@@ -19,49 +43,76 @@ const BookingForm = () => {
             molestie vel, ornare non id blandit netus.
           </p>
         </div>
+
         {/* booking form */}
         <div className="w-full lg:w-[635px]">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-[30px] mb-4 lg:mb-6 ">
+              {/* name input */}
               <input
                 type="text"
                 name="name"
+                onChange={handleChange}
                 placeholder="Your Name*"
-                className="border border-white-border w-full bg-inherit py-3 px-4 placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
+                className="border border-white-border w-full bg-inherit py-3 px-4 text-white-solid placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
               />
+
+              {/* email input */}
               <input
                 type="email"
                 name="email"
+                onChange={handleChange}
                 placeholder="Your Email"
-                className="border border-white-border w-full bg-inherit py-3 px-4 placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
+                className="border border-white-border w-full bg-inherit py-3 px-4 text-white-solid  placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
               />
             </div>
-            {/*  */}
+
+            {/*  date and people*/}
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-[30px] mb-4 lg:mb-6 ">
-              <input
-                type="date"
-                name="date"
-                placeholder="Reservation Date"
-                className="border border-white-border w-full bg-inherit py-3 px-4 caret-white-solid focus:text-gray-400 outline-none text-white-solid"
-              />
+              <div className="relative w-full">
+                {/* date */}
+                <input
+                  type="date"
+                  name="date"
+                  onChange={handleChange}
+                  placeholder="Reservation Date"
+                  className="border border-white-border w-full bg-inherit py-3 px-4 caret-white-solid outline-none text-white-solid appearance-none"
+                />
+                <span className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                  <img
+                    src={calender}
+                    alt="Calendar Icon"
+                    className="w-5 h-5 "
+                  />
+                </span>
+              </div>
+
+              {/* people */}
+
               <input
                 type="number"
                 name="people"
+                onChange={handleChange}
                 placeholder="Total People*"
-                className="border border-white-border w-full bg-inherit py-3 px-4 text-white-solid  caret-white-solid outline-none"
+                className="border border-white-border w-full bg-inherit py-3 px-4 text-white-solid  placeholder-white-solid  caret-white-solid outline-none"
               />
             </div>
+
+            {/* message box */}
             <div>
               <textarea
                 name="Message"
-                id=""
-                cols="40"
                 rows="7"
+                onChange={handleChange}
                 placeholder="Message"
-                className="border border-white-border w-full bg-inherit py-3 px-4 placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
+                className="border border-white-border w-full bg-inherit py-3 px-4 text-white-solid  placeholder-white-solid  caret-white-solid  focus:placeholder-gray-400 outline-none"
               ></textarea>
             </div>
-            <button className="mt-8">Book Now</button>
+
+            {/* book now btn */}
+            <button type="submit" className="mt-8">
+              Book Now
+            </button>
           </form>
         </div>
       </div>
