21776c38e311f1ca4cfbdcefd8e43e6c03f7b5f2
Afsana Meem <afsumeem@gmail.com>
2024-11-09 05:06:51 +0600

    slider navigation updated

--- src/components/ui/SliderNavigation.jsx
+++ src/components/ui/SliderNavigation.jsx
@@ -14,25 +14,29 @@ const SliderNavigation = () => {
   };
 
   return (
-    <div className="navigationContainer flex gap-8 justify-center mx-auto mt-4">
+    <div className="navigationContainer flex gap-8 my-6 justify-center mx-auto mt-4">
       {/* prev arrow */}
 
       <button
         onClick={handlePrevClick}
-        className="bg-white-solid w-[40px] h-[40px] rounded-full shadow-navigation-arrow flex items-center justify-center lg:h-[60px] lg:w-[60px] "
+        className="bg-white-solid w-[40px] h-[40px] rounded-full shadow-navigation-arrow flex items-center justify-center lg:h-[60px] lg:w-[60px] p-[10px]"
       >
-        <img src={leftArrow} alt="" className="h-[11px] w-[19px] " />
+        <img
+          src={leftArrow}
+          alt=""
+          className=" max-w-[7.18px] max-h-[12.47px] lg:h-[11px] lg:w-[19px] "
+        />
       </button>
 
       {/* next arrow */}
       <button
         onClick={handleNextClick}
-        className="bg-white-solid w-[40px] h-[40px] rounded-full shadow-navigation-arrow flex items-center justify-center lg:h-[60px] lg:w-[60px] "
+        className="bg-white-solid w-[40px] h-[40px] rounded-full shadow-navigation-arrow flex items-center justify-center lg:h-[60px] lg:w-[60px] p-[10px]"
       >
         <img
           src={rightArrow}
           alt=""
-          className="h-[7px] w-[12px] lg:h-[11px] lg:w-[19px] "
+          className="max-w-[7.18px] max-h-[12.47px] lg:h-[11px] lg:w-[19px] "
         />
       </button>
     </div>
